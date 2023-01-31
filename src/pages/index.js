bcfccd348b0d6d899aab84f754ea5e259d32f7a6
Ektagola <ektag8097@gmail.com>
2023-01-31 18:15:41 +0530

    next-app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import Banner from "./sections/banner";
 import Categories from "./sections/categories";
 import Delivery from "./sections/delivery";
-import Order from "./sections/order";
 import Serve from "./sections/serve";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +17,7 @@ export default function Home() {
         </title>
         <meta name="description" content="Best and unique food recipes" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/short_logo.png" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Banner />
       <Categories />
